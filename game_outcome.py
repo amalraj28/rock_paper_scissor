bac141eacea836bda63f2870ef62ae7616062184
Amal Raj <99964089+amalraj28@users.noreply.github.com>
2022-05-21 01:11:56 +0530

    Update game_outcome.py

--- game_outcome.py
+++ game_outcome.py
@@ -14,16 +14,18 @@ def outcome(random_symbol, user):
     print(f'You showed {user}')
     print(drawings.image(user))     # Prints image corresponding to user input
 
-    key = winning_combo[random_symbol]  # Winning combination of randomly chosen element is stored in the variable "key".
+    value = winning_combo[random_symbol]  # Winning combination of randomly chosen element is stored in the variable "value".
 
     """
     The idea applied here is that only one winning combination exists for each of the 3 values.
     These winning combinations are stored as a key-value pair in dictionary.
-    So compare the user inputted values with these key-value pairs and decide the winner.
+    So compare the user inputted values with these key-value pairs and decide the winner. Here the random symbol generated is taken as key of winning_combo dictionary.
+    If the random symbol and user input are same, the game is drawn. Otherwise if winning combination of random symbol and user input are same, it means the user has lost
+    If the above conditions fail, then user wins
     """
     if random_symbol == user:
         print('Result: Game drawn!')
-    elif key == user:
+    elif value == user:
         print('Result: You lost!')
     else:
         print('Result: You won!!!')
